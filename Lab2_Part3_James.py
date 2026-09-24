679dd7ef7a19792b246fed983397a1dacd91b10a
James C <james.s.conover@gmail.com>
2026-09-24 10:09:29 -0400

    Add files via upload

---
+++ Lab2_Part3_James.py
@@ -0,0 +1,13 @@
+#this is the part 3 of lab 2 to find the distance between them#
+import math
+
+x1=float(input('Enter x-coordinate for Point 1:'))
+y1=float(input('Enter y-coordinate for Point 1:'))
+x2=float(input('Enter x-coordinate for Point 2:'))
+y2=float(input('Enter y-coordinate for Point 2:'))
+
+total=math.sqrt((x2-x1)**2+(y2-y1)**2)
+total=format(float(total),".2f")
+print('The distance between the two points:',total)
+
+
